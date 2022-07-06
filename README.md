ade55d9375c689638d71478e210474b35ead5fb2
maria carrero <mariacarrerochacon267@gmail.com>
2022-07-06 15:39:23 -0400

    changes notes and layout component

--- README.md
+++ README.md
@@ -11,5 +11,11 @@
 3. Create Next App
    1. npx create-next-app
    2. add @mui library
+
 4. Publish to github
    1. push to github from vscode
+
+5. Create Website Layout 
+    1. Add header
+    2. Add main
+    3. Add footer
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,8 @@
-import { Typography } from "@mui/material";
 import Head from "next/head";
+import Layout from "../Components/Layout";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Sanity Amazona</title>
-        <meta
-          name="description"
-          content="The ecommerce website by next and sanity"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant="h1">
-        Sanity Amazona
-      </Typography>
-    </div>
-  );
+  return( <Layout>
+    list of products
+    </Layout>);
 }
